82a291db8933e577eefd8ca189a93f6d1f1c9fd2
RugResistant <rugresistant@gmail.com>
2025-08-03 18:42:44 -0400

    Update TokenInsight.ts

--- src/types/TokenInsight.ts
+++ src/types/TokenInsight.ts
@@ -1,1 +1,29 @@
+// src/types/TokenInsight.ts
+export interface TokenInsight {
+  token_mint: string;
+  name: string;
+  symbol: string;
+  price: number;
+  marketCap: number;
+  holders: number;
+  createdAt: string;
+  volume: number;
+  logs?: {
+    type: string;
+    message: string;
+    created_at: string;
+  }[];
+  ohlcv?: {
+    time: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+  }[];
+  swapVolumes?: {
+    time: string;
+    volume: number;
+  }[];
+}
 
